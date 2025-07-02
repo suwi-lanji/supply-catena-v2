ae91048a31cf4dc163ada4deaa720d72580ac410
Suwilanji Chipofya <138398395+suwi-lanji@users.noreply.github.com>
2025-07-02 09:12:15 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(
             at: '*',
-            headers: Request::HEADER_X_FORWARDED_ALL
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO |
+                     Request::HEADER_X_FORWARDED_AWS_ELB
         );
     })
     ->withExceptions(function (Exceptions $exceptions) {
