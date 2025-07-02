77d47abc81fab9a1bf79c74f46e48ded1f9ff42b
Suwilanji Chipofya <138398395+suwi-lanji@users.noreply.github.com>
2025-07-02 09:05:06 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(
-            proxies: '*',
+            at: '*',
             headers: Request::HEADER_X_FORWARDED_ALL
         );
     })
