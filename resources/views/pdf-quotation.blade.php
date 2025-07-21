5ad849fa798035f2e6b4bf340a9b832154b0f9e1
Suwilanji Chipofya <138398395+suwi-lanji@users.noreply.github.com>
2025-07-21 16:23:07 +0200

    Update pdf-quotation.blade.php

--- resources/views/pdf-quotation.blade.php
+++ resources/views/pdf-quotation.blade.php
@@ -1,317 +1,299 @@
 @php
-$customer = \App\Models\Customer::where("id", $record->customer_id)->first();
+$customer = \App\Models\Customer::where('id', $record->customer_id)->first();
 $tenant = \Filament\Facades\Filament::getTenant();
-$fullpath = base_path() . "/storage/app/public" . str_replace("/content/", "/", $tenant->logo);
+$fullpath = base_path() . '/storage/app/public' . str_replace('/content/', '/', $tenant->logo);
 @endphp
-@php
-        $terms = \App\Models\PaymentTerm::find($record->payment_term_id);
-        @endphp
 <style>
     body {
-    font-family: "Figtree", sans-serif;
-    margin: 0;
-    padding: 0;
-    font-size: 8px; /* Reduced font size for PDF */
-}
+        font-family: "Figtree", sans-serif;
+        margin: 0;
+        padding: 0;
+        font-size: 0.7em;
+    }
 
-.invoice {
-    box-sizing: border-box;
-}
+    .invoice {
+        box-sizing: border-box;
+    }
 
-.invoice-header, .invoice-footer {
-    margin-bottom: 20px;
-}
+    .invoice-header, .invoice-footer {
+        margin-bottom: 50px;
+    }
 
-.invoice-header-left img {
-    width: 100px; /* Adjusted for better fit */
-    height: 100px;
-    border-radius: 50%;
-}
+    .invoice-header-left img {
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+    }
 
-.invoice-header-right h5 {
-    margin: 0;
-    font-size: 10px; /* Reduced size for header text */
-}
+    .invoice-header-right h5 {
+        margin: 0;
+    }
 
-.invoice-header-right address, .invoice-footer p, .invoice-footer h5 {
-    margin: 0;
-    font-size: 8px; /* Reduced size for addresses and footer text */
-}
+    .invoice-header-right address, .invoice-footer p, .invoice-footer h5 {
+        margin: 0;
+    }
 
-.invoice-body {
-    margin-bottom: 20px;
-}
+    .invoice-body {
+        margin-bottom: 50px;
+    }
+    .clearfix::after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+        .left, .right {
+            box-sizing: border-box;
+        }
+        .left {
+            float: left;
+            width: 45%;
+        }
+        .right {
+            float: right;
+            width: 45%;
+            text-align: right;
+        }
+    .invoice-table {
+        width: 48%;
+        border-collapse: collapse;
+    }
 
-.clearfix::after {
-    content: "";
-    display: table;
-    clear: both;
-}
+    .invoice-table th, .invoice-table td {
+        border: 1px solid #ccc;
+        padding: 3px;
+        text-align: left;
+    }
 
-.left, .right {
-    box-sizing: border-box;
-    width: 48%;
-}
+    .invoice-columns {
+        display: flex;
+        justify-content: space-between;
+    }
 
-.left {
-    float: left;
-}
+    .terms-column,
+    .total-column {
+        width: 48%;
+        padding-top: 70px;padding-bottom: 10px;
+    }
 
-.right {
-    float: right;
-    text-align: right;
-}
+    ul {
+        list-style: none;
+        padding: 0;
+    }
 
-.invoice-table {
-    width: 100%; /* Adjusted to use full width */
-    border-collapse: collapse;
-}
+    .table-sm .action-icon {
+        font-size: 1rem;
+    }
 
-.invoice-table th, .invoice-table td {
-    border: 1px solid #ccc;
-    padding: 2px; /* Reduced padding */
-    font-size: 7px; /* Smaller font size for table */
-}
+    .table-sm>:not(caption)>*>* {
+        padding: .2rem .2rem;
+    }
 
-.invoice-columns {
-    display: flex;
-    justify-content: space-between;
-}
+    .bg-light-subtle {
+        background-color: #fcfcfd !important;
+    }
 
-.terms-column,
-.total-column {
-    width: 48%;
-    padding-top: 10px; /* Reduced padding */
-}
+    .border-light {
+        --ct-border-opacity: 1;
+        border-color: rgba(242, 242, 247, 1) !important;
+    }
 
-ul {
-    list-style: none;
-    padding: 0;
-    font-size: 7px; /* Smaller font size for list items */
-}
+    .mb-0 {
+        margin-bottom: 0 !important;
+    }
 
-.table-sm .action-icon {
-    font-size: 0.8rem;
-}
+    .mt-3 {
+        margin-top: 1.5rem !important;
+    }
 
-.table-sm>:not(caption)>*>* {
-    padding: 0.2rem;
-}
+    .table-dark {
+        color: #fff;
+        background-color: #212529;
+        border-color: #373b3e;
+    }
 
-.bg-light-subtle {
-    background-color: #fcfcfd !important;
-}
+    .h5, h5 {
+        font-size: .91rem;
+    }
 
-.border-light {
-    --ct-border-opacity: 1;
-    border-color: rgba(242, 242, 247, 1) !important;
-}
+    .table-centered td, .table-centered th {
+        vertical-align: middle !important;
+    }
 
-.mb-0 {
-    margin-bottom: 0 !important;
-}
+    .table-borderless>:not(caption)>*>* {
+        border-bottom-width: 0;
+    }
 
-.mt-3 {
-    margin-top: 1.5rem !important;
-}
+    tbody, td, tfoot, th, thead, tr {
+        border-color: inherit;
+        border-style: solid;
+        border-width: 0;
+    }
 
-.table-dark {
-    color: #fff;
-    background-color: #212529;
-    border-color: #373b3e;
-}
+    table {
+        border-collapse: collapse;
+    }
 
-.h5, h5 {
-    font-size: 9px; /* Reduced font size for h5 */
-}
-
-.table-centered td, .table-centered th {
-    vertical-align: middle !important;
-}
-
-.table-borderless>:not(caption)>*>* {
-    border-bottom-width: 0;
-}
-
-tbody, td, tfoot, th, thead, tr {
-    border-color: inherit;
-    border-style: solid;
-    border-width: 0;
-}
-
-table {
-    border-collapse: collapse;
-}
-
-.table {
-    width: 100%;
-    margin-bottom: 1.5rem;
-    color: var(--ct-table-color);
-    vertical-align: top;
-    border-color: var(--ct-table-border-color);
-}
-
-th {
-    text-align: left;
-    font-size: 7px; /* Reduced font size for table headers */
-}
-
-span, p {
-    font-size: 8px; /* Smaller font size for general text */
-}
-
-
+    .table {
+        width: 100%;
+        margin-bottom: 1.5rem;
+        color: var(--ct-table-color);
+        vertical-align: top;
+        border-color: var(--ct-table-border-color);
+    }
+    th {
+      text-align:left;
+    }
 </style>
 <div class="invoice">
     <div class="invoice-header clearfix">
         <div class="left">
-            <img src="data:image/png;base64,{{ base64_encode(file_get_contents($fullpath)) }}" alt="Logo" style="width: 100px; height: 100px; display: inline-block;"/>
-            <div style="display: inline-block;height: 100px; vertical-align: top;">
-                <h5>{{ $tenant->portal_name }}</h5>
-                <address>
-                    <span>{{ $tenant->email }}</span><br/>
-                    <span>{{ $tenant->street_1 }}</span><br/>
-                    <span>{{ $tenant->city }}, {{ $tenant->province }}, {{ $tenant->business_location }}</span><br/>
-                    <abbr title="Phone">Phone:</abbr> <span>{{ $tenant->phone }}</span>
-                </address>
-            </div>
+        <img src="data:image/png;base64,{{ base64_encode(file_get_contents($fullpath)) }}" alt="Logo" style="width: 150px; height: 150px;display:inline-block;"/>
         </div>
-        <div class="right" style="width: 50%;">
-            <h5>QUOTATION</h5>
-            <p><strong>Bank:</strong> {{ $terms->bank }}</p>
-            <p><strong>A/C Name:</strong> {{ $terms->account_name }}</p>
-            <p><strong>Account No:</strong> {{ $terms->account_number }}</p>
-            <p><strong>Branch:</strong> {{ $terms->branch }}</p>
-            <p><strong>Swift Code:</strong> {{ $terms->swift_code }}</p>
-            <p><strong>Branch No:</strong> {{ $terms->branch_number }}</p>
+        <div class="right">
+            <h5>{{ $tenant->portal_name }}</h5>
+            <address>
+                <span>{{ $tenant->email }}</span><br/>
+                <span>{{ $tenant->street_1 }}</span><br/>
+                <span>{{ $tenant->city }}, {{ $tenant->province }}, {{ $tenant->business_location }}</span><br/>
+                <abbr title="Phone">Phone:</abbr> <span>{{ $tenant->phone }}</span>
+            </address>
         </div>
     </div>
 
     <div class="invoice-tables clearfix">
         <table class="invoice-table left">
-            <thead>
-                <tr>
-                    <th colspan="2">DELIVERY ADDRESS</th>
-                </tr>
-            </thead>
             <tbody>
+                <tr>
+                    <th class="text-nowrap">TPIN/Account No.</th>
+                    <td>{{ $customer['tpin'] }}</td>
+                </tr>
                 <tr>
                     <td colspan="2">
                         <address>
-                            <span>{{ $customer["company_display_name"] }}</span><br/>
-                            <span>{{ $customer["billing_street_1"] }}, {{ $customer["billing_city"] }}, {{ $customer["billing_country"] }}</span><br/>
-                            <span>{{ $customer["billing_city"] }}</span><br/>
-                            <abbr title="Phone">Phone:</abbr> <span>{{ $customer["phone"] }}</span>
+                            <span>{{ $customer['company_display_name'] }}</span><br/>
+                            <span>{{ $customer['billing_street_1'] }}, {{ $customer['billing_city'] }}, {{ $customer['billing_country'] }}</span><br/>
+                            <span>{{ $customer['billing_city'] }}</span><br/>
+                            <abbr title="Phone">Phone:</abbr> <span>{{ $customer['phone'] }}</span>
                         </address>
                     </td>
                 </tr>
                 <tr>
-                    <th>Contact No.</th>
-                    <td>{{ $customer["phone"] }}</td>
+                    <th class="text-nowrap">Contact No.</th>
+                    <td>{{ $customer['phone'] }}</td>
                 </tr>
                 <tr>
-                    <th>Email Address</th>
-                    <td>{{ $customer["email"] }}</td>
+                    <th class="text-nowrap">Email Address</th>
+                    <td>{{ $customer['email'] }}</td>
                 </tr>
                 <tr>
-                    <th>VAT No.</th>
+                    <th class="text-nowrap">VAT No.</th>
                     <td>N/A</td>
                 </tr>
             </tbody>
         </table>
-    </div>
-    <div>
-        <table>
-            <thead>
+        <table class="invoice-table right">
+            <thead class="table-dark">
                 <tr>
-                    <th>VALID DATE</th>
-                    <th>CLIENT</th>
-                    <th>REPORT NO.</th>
-                    <th>INCO TERM</th>
-                    <th>STOCK IN</th>
-                    <th>LEAD TIME</th>
-                    <th>PAYMENT TERM</th>
+                    <td colspan="2" class="text-center">Sales Order</td>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>01/09/24-22/09/24</td>
-                    <td>KCC</td>
-                    <td>JOBCARD0007</td>
-                    <td>DDP MINSTE</td>
-                    <td>EX STOCK</td>
-                    <td>20 - 35 WORKING DAYS</td>
-                    <td>30 DAYS</td>
+                    <td>Quotation No.</td>
+                    <td>{{$record->quotation_number}}</td>
+                </tr>
+                <tr>
+                    <td>Quotation Date</td>
+                    <td>{{$record->quotation_date}}</td>
+                </tr>
+                <tr>
+
+                    <td>Prepared By.</td>
+                    <td>
+                      @php
+                      echo \App\Models\SalesPerson::where('id', $record->sales_person_id)->pluck('email')->first();
+                      @endphp
+                    </td>
                 </tr>
             </tbody>
         </table>
     </div>
+
     <div class="invoice-body">
         <table class="table table-sm table-centered table-hover table-borderless mb-0 mt-3">
-            <thead>
+            <thead class="border-top border-bottom border-light">
                 <tr>
-                    <th>ITEM</th>
-                    <th>MATERIAL NO.</th>
-                    <th>PART NO.</th>
-                    <th>DESCRIPTION</th>
-                    <th>LEAD TIME</th>
-                    <th>QTY</th>
-                    <th>UNIT PRICE (EXCL)</th>
-                    <th>UNIT PRICE (INCL)</th>
-                    <th>DISC %</th>
-                    <th>TOTAL PRICE (EXCL)</th>
+                    <th>Qty</th>
+                    <th>Part No.</th>
+                    <th>Description</th>
+                    <th>Condition</th>
+                    <th>Weight</th>
+                    <th>Lead Time</th>
+                    <th>Unit Price</th>
+                    <th>Discount</th>
+                    <th>Amount</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($record->items as $index => $item)
                 <tr>
-                    <td>{{ $index + 1 }}</td>
-                    <td>{{ Arr::get($item, "sku", 0) }}</td>
-                    <td>{{ \App\Models\Item::where("id", $item["item"])->pluck("part_number")->first() }}</td>
-                    <td>{{ \App\Models\Item::where("id", $item["item"])->pluck("description")->first() }}</td>
-                    <td>{{ $item["lead_time"] }}</td>
-                    <td>{{ Arr::get($item, "quantity", 0) }}</td>
-                    <td>{{$tenant->currency_symbol}}{{ $item["rate"] }}</td>
-                    <td>{{$tenant->currency_symbol}}{{ $item["rate"] }}</td>
-                    <td>{{ Arr::get($item, "discount", 0) . "%" }}</td>
-                    <td>{{$tenant->currency_symbol}}{{ $item["amount"] }}</td>
+                    <td>@php
+                            echo Arr::get($item, 'quantity', 0);
+                        @endphp</td>
+                    <td>{{ \App\Models\Item::where('id', $item['item'])->pluck('part_number')->first() }}</td>
+                    <td>{{ \App\Models\Item::where('id', $item['item'])->pluck('description')->first() }}</td>
+                    <td>{{ \App\Models\Item::where('id', $item['item'])->pluck('condition')->first() }}</td>
+                    <td>{{$item['weight']}}</td>
+                    <td>{{$item['lead_time']}}</td>
+                    <td>{{$item['rate']}}</td>
+                    <td>
+                        @php
+                            echo Arr::get($item, 'discount', 0) . "%";
+                        @endphp
+                    </td>
+                    <td>{{$item['amount']}}</td>
                 </tr>
                 @endforeach
+                <tr>
+                    <td colspan="5"></td>
+                    @php
+                    $totalWeight = array_reduce($record->items, function($carry, $item) {
+                        return $carry + $item['weight'];
+                    }, 0);
+                    @endphp
+                    <td><b>Total Weight: {{$totalWeight}}</b></td>
+                    <td colspan="3"></td>
+                </tr>
             </tbody>
         </table>
-        <div class="clearfix">
-            <table class="left">
-                <thead>
-                    <tr>
-                        <th>QUOTATION NO.</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{{$tenant->quotation_number}}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <table class="right">
-                <thead>
-                    <tr>
-                        <th>TOTAL</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @php
-                        $totalAmount = 0
-                    @endphp
-                    <tr>
-                        <td>{{$tenant->currency_symbol}}{{ number_format($record->sub_total, 2) }}</td>
-                    </tr>
-                </tbody>
-            </table>
+    </div>
+
+    <div class="invoice-columns clearfix">
+        <div class="terms-column left">
+            <h4>Terms and Conditions</h4>
+            <ul>
+                @foreach ($record->terms_and_conditions as $index => $t_and_c)
+                <li><small>{{ $t_and_c['terms_and_conditions'] }}</small></li>
+                @endforeach
+            </ul>
+        </div>
+        <div class="invoice-footer right">
+            @php
+            $totalVat = array_reduce($record->items, function($carry, $item) {
+                return $carry + Arr::get($item, 'tax', 0);
+            }, 0);
+            $totalDiscount = array_reduce($record->items, function($carry, $item) {
+                return $carry + Arr::get($item, 'discount', 0);
+            }, 0);
+            $totalDiscount += floatval($record->discount);
+            @endphp
+            <p><b>Sub-total: </b><span class="float-end">{{$tenant->currency_symbol}}{{ $record->sub_total }}</span></p>
+            <p><b>Discount ({{$totalDiscount}}%):</b> <span class="fw-normal text-body">{{$totalDiscount}}%</span></p>
+            <p><b>VAT ({{$totalVat}}%):</b> <span class="fw-normal text-body">{{$totalVat}}%</span></p>
+            <h5>{{$tenant->currency_symbol}}{{$record->total}}  {{$tenant->currency_code}}</h5>
         </div>
     </div>
-    <div class="invoice-footer">
-        <p>{{ $terms->notes }}</p>
-    </div>
+
     <div style="margin-bottom: 50px">
         @php
         $terms = \App\Models\PaymentTerm::find($record->payment_term_id);
