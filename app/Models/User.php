ac90a55a52efd75de57e5e29388ff9d9a413d8bf
Z User <z@container>
2026-04-17 02:09:44 +0000

    fix: add HasRoles trait to User model for spatie/laravel-permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,10 +12,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Collection;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser, HasTenants
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     protected $teamForeignKey = 'team_id';
 
